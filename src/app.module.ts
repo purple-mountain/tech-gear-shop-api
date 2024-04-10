2124bf6c828d48ccb3fd477a5c46f61bcc3f47b7
purple-mountain <lmwrong92@gmail.com>
2024-04-10 22:07:19 +0500

    feat: add auth strategy for auth0

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
 import { DbModule } from "./db/db.module";
+import { AuthModule } from "./auth/auth.module";
 
 @Module({
-    imports: [ConfigModule.forRoot(), DbModule],
+    imports: [ConfigModule.forRoot(), DbModule, AuthModule],
     controllers: [AppController],
     providers: [AppService],
 })
