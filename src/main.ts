b80cb6d4a795336e9177c709d62a4c8cced27d48
purple-mountain <lmwrong92@gmail.com>
2024-04-10 22:44:46 +0500

    feat: add zod validation pipe and filter

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { ZodValidationFilter } from "./common/exceptions/zod-validation.filter";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.enableCors();
+    app.useGlobalFilters(new ZodValidationFilter());
     await app.listen(3000);
 }
 bootstrap();
