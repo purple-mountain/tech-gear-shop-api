6638c92084581ce96be115fb2b74038400e8a0ca
purple-mountain <lmwrong92@gmail.com>
2024-04-17 15:43:47 +0500

    feat/refactor: add redis caching and change folder structure

---
+++ src/providers/cache/redis/redis-cache.module.ts
@@ -0,0 +1,21 @@
+import { Module } from "@nestjs/common";
+import { CacheModule } from "@nestjs/cache-manager";
+import { redisStore } from "cache-manager-redis-yet";
+import { RedisClientOptions } from "redis";
+import { RedisCacheService } from "./redis-cache.service";
+
+@Module({
+    imports: [
+        CacheModule.register<RedisClientOptions>({
+            isGlobal: true,
+            store: redisStore,
+            socket: {
+                host: process.env.REDIS_HOST ?? "localhost",
+                port: parseInt(process.env.REDIS_PORT ?? "6379"),
+            },
+            password: process.env.REDIS_PASSWORD ?? "root",
+        }),
+    ],
+    exports: [RedisCacheService],
+})
+export class RedisCacheModule {}
